da848351353e1a098b67ac564ebf28a90e8bbd7c
elenalape <elenalape@github.com>
2020-05-29 02:51:16 +0100

    added transloadit integration

--- src/components/UppyComponent.js
+++ src/components/UppyComponent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Uppy from "@uppy/core";
-import Tus from "@uppy/tus";
+// import Tus from "@uppy/tus";
+import Transloadit from "@uppy/transloadit";
 import "@uppy/core/dist/style.css";
 import "@uppy/dashboard/dist/style.css";
 import { Dashboard } from "@uppy/react";
@@ -20,12 +21,52 @@ class UppyComponent extends React.Component {
 		});
 
 		// TODO: so how do we send this to Transloadit?
-		this.uppy.use(Tus, { endpoint: "https://master.tus.io/files/" });
+		// this.uppy.use(Tus, { endpoint: "https://master.tus.io/files/" });
 
-		this.uppy.on("complete", (result) => {
-			console.log("Completed upload, result:", result);
-			const id = result.successful[0].id;
-			const url = result.successful[0].uploadURL;
+		this.uppy.use(Transloadit, {
+			params: {
+				auth: {
+					key: "9338010312764ec2aa950e5787982660",
+				},
+				// It’s more secure to use a template_id and enable
+				// Signature Authentication
+				steps: {
+					":original": {
+						robot: "/upload/handle",
+					},
+					video_resized: {
+						use: ":original",
+						robot: "/video/encode",
+						result: true,
+						ffmpeg_stack: "v3.3.3",
+						resize_strategy: "fit",
+						height: 100,
+						preset: "ipad-high",
+						width: 150,
+					},
+					invert_watermarked: {
+						use: ":original",
+						robot: "/video/encode",
+						result: true,
+						ffmpeg_stack: "v3.3.3",
+						height: 100,
+						resize_strategy: "fillcrop",
+						preset: "ipad-high",
+						watermark_position: "top-left",
+						watermark_size: "100%",
+						watermark_url:
+							"https://demos.transloadit.com/inputs/waterframe.png",
+						width: 150,
+					},
+				},
+			},
+			waitForEncoding: true,
+		});
+
+		this.uppy.on("transloadit:result", (stepName, result) => {
+			console.log("Result:", result);
+			const id = result.id;
+			const url = result.url;
 			handleUploadCompleted(id, url);
 		});
 
